59ac9f6162c3c36f87d7e9d2ce6e37cc3066d24f
antonysantos71 <ribeiroantony02.56@gmail.com>
2025-07-03 14:00:52 -0300

    primeiras linhas e primeiros estilos

---
+++ src/app/index/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View, StyleSheet } from "react-native";
+import { styles } from "./styles";
+
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hellou World Native!</Text>
+      <Text style={styles.title}>Hellou Native!</Text>
+    </View>
+  );
+}
+
+
