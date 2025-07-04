06b286d87535cc46c0223fbd98c96dd3ad82a7e0
antonysantos71 <ribeiroantony02.56@gmail.com>
2025-07-04 14:05:25 -0300

    criando componentes, category

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,6 +2,7 @@ import { Image, TouchableOpacity, View } from "react-native"; //componentes do p
 import { styles } from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
+import { Category } from "@/components/category";
 
 export default function Index() {
   return (
@@ -12,6 +13,7 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+      <Category />
     </View>
   );
 }
