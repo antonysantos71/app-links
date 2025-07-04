88eb6c5090a23386f44e10d2cf163d31166cc6b7
antonysantos71 <ribeiroantony02.56@gmail.com>
2025-07-04 14:26:09 -0300

    tipagens & props, category

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,13 +1,18 @@
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
-import { Pressable, Text } from "react-native";
+import { Pressable, Text, PressableProps } from "react-native"; // tipagens do react native
 import { styles } from "./styles";
 
-export function Category() {
+interface Categories extends PressableProps {
+  name: string;
+  icon: keyof typeof MaterialIcons.glyphMap;
+}
+
+export function Category({ name, icon, ...props }: Categories) {
   return (
-    <Pressable style={styles.container}>
-      <MaterialIcons name="code" size={16} color={colors.gray[400]}/>
-      <Text style={styles.name}>Projetos</Text>
+    <Pressable style={styles.container} {...props}>
+      <MaterialIcons name={icon} size={16} color={colors.gray[400]} />
+      <Text style={styles.name}>{name}</Text>
     </Pressable>
-  )
+  );
 }
